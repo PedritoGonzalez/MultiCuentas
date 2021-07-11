207b96b1b3833e5ee2e9ed0c5abb8a68a0c96ab1
PedritoGonzalez <86697444+PedritoGonzalez@users.noreply.github.com>
2021-07-11 18:08:24 -0300

    Add files via upload

---
+++ tools/intento.py
@@ -0,0 +1,41 @@
+import mechanize
+import sys
+import random
+from mechanize import Browser
+
+nombres = ['PedroZ', 'Catalina', 'Tomas', 'Delfina', 'Renata', 'Santiago', 'Matias', 'Sofia', 'Lucia', 'Pilar', 'Ignacio']
+class intentoHilo:
+        
+    def __init__(self, url):
+        
+        self.bandera = False
+        self.url = url
+
+    #Hilo principal de intentos
+
+    def comenzar(self):
+
+        self.bandera = True
+        
+        browser = Browser()
+        
+        while(self.bandera):
+
+            try:
+                         
+                browser.open(self.url)
+                browser.select_form(nr = 0)                
+                browser.form['username'] = random.choice(nombres)
+                browser.form['password'] = ""
+                browser.submit()
+
+            except err:
+
+                print("Error ")
+
+
+
+
+
+
+        
